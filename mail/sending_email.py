9f568ced5eea2ca9bbcec52922904e980a6f16e6
bhanvi <42137926+bhanvimenghani@users.noreply.github.com>
2020-05-30 02:02:01 +0530

    Update sending_email.py

--- mail/sending_email.py
+++ mail/sending_email.py
@@ -1,12 +1,12 @@
 import smtplib
 import urllib.request as urllib
-sender_email = "menghanibhanvi@gmail.com"
-rec_email = "menghanibhannvi@gmail.com"
+sender_email = "email@gmail.com"
+rec_email = "emai@gmail.com"
 
 message = """ The Model has been created glad ! """
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("menghanibhanvi@gmail.com", "jack3141")
+server.login("email@gmail.com", "password")
 print("Login Success!")
-server.sendmail("bhanvi", "menghanibhanvi@gmail.com", message)
+server.sendmail("bhanvi", "email@gmail.com", message)
 print(f"Email has been sent successfully to {rec_email}")
